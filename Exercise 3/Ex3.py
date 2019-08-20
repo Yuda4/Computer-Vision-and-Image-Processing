cc225349e147eabbf68ecb51e617b00cbf717047
Yehuda Neumann <33513480+Yuda4@users.noreply.github.com>
2019-08-20 21:39:58 +0300

    Update Ex3.py

--- Exercise 3/Ex3.py
+++ Exercise 3/Ex3.py
@@ -66,4 +66,59 @@ def LaplacianToImage(lpyr):
 
     return reconstructed_image
     
+def pyramidBlending(img1, img2, mask, maxLevels):
+    img1 = cv2.resize(img1, (img1.shape[1], img1.shape[1]))
+    img2 = cv2.resize(img2, (img1.shape[1], img1.shape[1]))
+
+    mask = cv2.GaussianBlur(mask, (21,21),11 )
+
+    # Gaussian Pyramid 1
+    layer = img1.copy()
+    gaussian_pyramid = [layer]
+    for i in range(maxLevels):
+        layer = cv2.pyrDown(layer)
+        gaussian_pyramid.append(layer)
+
+    # Laplacian Pyramid 1
+    layer = gaussian_pyramid[maxLevels-1]
+    laplacian_pyramid = [layer]
+    for i in range(maxLevels-1, 0, -1):
+        size = (gaussian_pyramid[i - 1].shape[1], gaussian_pyramid[i - 1].shape[0])
+        gaussian_expanded = cv2.pyrUp(gaussian_pyramid[i], dstsize=size)
+        laplacian = cv2.subtract(gaussian_pyramid[i - 1], gaussian_expanded)
+        laplacian_pyramid.append(laplacian)
+
+    # Gaussian Pyramid 2
+    layer = img2.copy()
+    gaussian_pyramid2 = [layer]
+    for i in range(maxLevels-1):
+        layer = cv2.pyrDown(layer)
+        gaussian_pyramid2.append(layer)
+
+    # Laplacian Pyramid 2
+    layer = gaussian_pyramid2[maxLevels-1]
+    laplacian_pyramid2 = [layer]
+    for i in range(maxLevels-1, 0, -1):
+        size = (gaussian_pyramid2[i - 1].shape[1], gaussian_pyramid2[i - 1].shape[0])
+        gaussian_expanded = cv2.pyrUp(gaussian_pyramid2[i], dstsize=size)
+        laplacian = cv2.subtract(gaussian_pyramid2[i - 1], gaussian_expanded)
+        laplacian_pyramid2.append(laplacian)
+
+    # Laplacian Pyramid result
+    result_pyramid = []
+    for img1_lap, img2_lap in zip(laplacian_pyramid, laplacian_pyramid2):
+        if mask.ndim==3 and mask.shape[-1] == 3:
+            alpha = mask/255.0
+        else:
+            alpha = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)/255.0
+        blended = cv2.convertScaleAbs(img1*(1-alpha) + img2*alpha)
+
+    result_pyramid.append(blended)
+    result = result_pyramid[0]
+    cv2.imshow("Result image", result)
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+    imgFinal = Image.fromarray(result)
+    imgFinal.save('blended_image.png')
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
